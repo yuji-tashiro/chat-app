99507d5f289e8b79c9b11c170db2039ec65f0397
yuji-tashiro <yuji.tashiro1997@gmail.com>
2024-02-06 08:54:41 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_05_080749) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_05_234845) do
   create_table "answers", charset: "utf8mb4", force: :cascade do |t|
     t.integer "question_id"
     t.text "content"
@@ -23,6 +23,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_05_080749) do
     t.string "title"
     t.text "content"
     t.string "author_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
